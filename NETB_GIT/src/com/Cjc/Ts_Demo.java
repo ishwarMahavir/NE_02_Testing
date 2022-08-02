d2a256cb9a55164f703ef1869e85f05b1b461211
ishwarMahavir <110445304+ishwarMahavir@users.noreply.github.com>
2022-08-03 04:50:34 +0530

    by Git

--- NETB_GIT/src/com/Cjc/Ts_Demo.java
+++ NETB_GIT/src/com/Cjc/Ts_Demo.java
@@ -3,7 +3,9 @@ package com.Cjc;
 public class Ts_Demo 
 {
 	public void m1()
+	{
 		System.out.println("Hello");
+	}
 	public static void main(String[] args) 
 	{
 	   System.out.println("Hello World");	
